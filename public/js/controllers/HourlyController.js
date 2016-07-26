7d85dd2c13cf91e49130bad24b8d9e22cafe848d
Jon Church <me@jonchurch.com>
2016-07-25 20:56:06 -0400

    completed skeleton of hourlyView and fixed the data bug in the hourly controller :cry:

---
+++ public/js/controllers/HourlyController.js
@@ -0,0 +1,17 @@
+(function() {
+    angular.module('ngWeather')
+        .controller('HourlyController', HourlyController);
+
+    HourlyController.$inject = ['$scope', 'WeatherService'];
+   
+    function HourlyController($scope, WeatherService) {
+        $scope.hourlyData = WeatherService.weather;
+        $scope.$watch(function(){
+            return WeatherService.weather;
+        }, function(value){
+            $scope.hourlyData = value;
+            console.log(value);
+        });
+    }
+
+})();
